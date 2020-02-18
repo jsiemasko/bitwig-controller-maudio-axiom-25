e22871053fd6cdd5760630b06c4e2cfefd0b9721
Jonathan Siemasko <j.siemasko@neopost.com>
2020-02-17 23:16:55 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-Bitwig controller script for the M-Audio Axiom 25
+# Bitwig controller script for the M-Audio Axiom 25
 
+This script was written for a Axiom 25 on a modified version of factory preset 1, i.e. the state of the keyboard after a factory reset.
+
+## Installation
+See installation instructions: [Controller Script Installation Guide](https://www.bitwig.com/en/community/control_scripts/installation_guide)
+
+In order to use the encoder mappings a preset needs to be modified on the Hardware.  Instructions are included in the controller script help file.
+
+## Usage
+The script will provide 2 different Note Inputs, one from the keys and one from the drum pads.
+The encoders map to the macros of the main device on the current track.
+Transport controls are mapped as follows:
+| Control      | Mapping                    |
+| ------------ | -------------------------- |
+| Loop         | Show VST GUI               |
+| Rewind       | Select previous track      |
+| Fast Forward | Select next track          |
+| Stop         | Stop the transport         |
+| Play         | Play / Pause the transport |
+| Record       | Record on armed tracks     |
+
+## History
+### 1.0
+* Changed auto-detect name to match what my Axiom was showing.
+* Added Drum Pads as a separate note input.
+* Removed encoder toggle to CC mode.
+* Changed encoders to work in relative mode rather than sending absolute CC values.
+
+## Credits
+Forked from: https://github.com/teetrinkers/bitwig-controller-maudio-axiom-25
--- axiom-25.control.js
+++ axiom-25.control.js
@@ -1,129 +1,133 @@
-loadAPI(1);
-
-load("rotaries.js");
+loadAPI(10);
 
 host.defineController("M-Audio", "Axiom 25", "1.0", "436eb006-523d-4fd4-adf1-431af7803e53");
 host.defineMidiPorts(1, 1);
-host.addDeviceNameBasedDiscoveryPair(["USB Axiom 25 Port 1"], ["USB Axiom 25 Port 1"]);
+host.addDeviceNameBasedDiscoveryPair(["USB Axiom 25"], ["USB Axiom 25"]);
 host.defineSysexIdentityReply("F0 7E 7F 06 02 00 20 08 63 0E 18 03 20 31 30 30 F7");
 
-var CC_MIN = 1;
-var CC_MAX = 119;
+var ccToMacroNum = {
+    71: 0,
+    74: 1,
+    84: 2,
+     7: 3,
+    91: 4,
+    93: 5,
+     5: 6,
+    10: 7
+};
 
-var DEVICE_MACRO_COUNT = 8
-var ccToUserIndex = []
-
-var CC = {
+var transportCC = {
     LOOP: 20,
     REW: 21,
     FF: 22,
     STOP: 23,
     PLAY: 24,
     REC: 25
-}
-
-var PADS = {
-    C14: 36,
-    C15: 38,
-    C16: 46,
-    C17: 42,
-    C18: 50,
-    C19: 45,
-    C20: 51,
-    C21: 49
-}
+};
 
 function init() {
     host.getMidiInPort(0).setMidiCallback(onMidi);
+    setupSysex();    
+    transport = host.createTransport();
+    cursorTrack = host.createCursorTrack(2, 0);
+    cursorDevice = cursorTrack.getPrimaryDevice();
+    cursorControlPage = cursorDevice.createCursorRemoteControlsPage(8);
+    createNoteInputs();
+    setIndications();
+}
 
+function setupSysex()
+{
     // Print Sysex ID response.
     host.getMidiInPort(0).setSysexCallback(function(data) {println(data);});
     println("Sysex ID:");
-    sendSysex("F0 7E 7F 06 01 F7")
+    sendSysex("F0 7E 7F 06 01 F7");
+}
 
-    noteInput = host.getMidiInPort(0).createNoteInput("Axiom 25",
+/*
+ * Set the mapping indicator on all macros
+ */
+function setIndications()
+{
+    var numOfMacros = 8;
+    for(i = 0; i < numOfMacros; i++)
+        cursorControlPage.getParameter(i).setIndication(true);
+}
+
+/*
+ * Create the note inputs for Keys and Pads
+ */
+function createNoteInputs()
+{
+    var noteInput = host.getMidiInPort(0).createNoteInput("Keys",
         "80????", "90????", "B001??", "B002??", "B00B??", "B040??", "C0????", "D0????", "E0????");
     noteInput.setShouldConsumeEvents(false);
 
-    transport = host.createTransport();
-    cursorTrack = host.createCursorTrack(2, 0);
-
-    rotaries = new Rotaries(cursorTrack);
-
-    // Init user controls.
-    userControls = host.createUserControls(CC_MAX - CC_MIN + 1 - DEVICE_MACRO_COUNT);
-    var index = 0;
-    for (var cc = CC_MIN; cc < CC_MAX; cc++) {
-        if (!rotaries.isRotary(cc) && !withinRange(cc, CC.LOOP, CC.REC)) {
-            ccToUserIndex[cc] = index;
-            userControls.getControl(index).setLabel("CC" + cc);
-            index++;
-        }
-    }
+    var padInput = host.getMidiInPort(0).createNoteInput("Pads",
+        "89????", "99????", "B901??", "B902??", "B90B??", "B940??", "C9????", "D9????", "E9????");
+    padInput.setShouldConsumeEvents(false);
 }
 
-function isUserControlCC(cc) {
-    return cc < ccToUserIndex.length && ccToUserIndex[cc] != undefined;
-}
-
+/*
+ * Called on every MIDI message that is not used for a note input 
+ */
 function onMidi(status, data1, data2) {
-    // printMidi(status, data1, data2)
-
-    if (isChannelController(status)) {
-
-        // Transport buttons
-        switch (data1) {
-            case CC.LOOP:
-                cursorTrack.getPrimaryDevice().isWindowOpen().toggle();
-                break;
-            case CC.REW:
-                cursorTrack.selectPrevious();
-                break;
-            case CC.FF:
-                cursorTrack.selectNext();
-                break;
-            case CC.STOP:
-                transport.stop();
-                break;
-            case CC.PLAY:
-                transport.play();
-                break;
-            case CC.REC:
-                transport.record();
-                break;
+    if (isChannelController(status)) {        
+        var macroNum = ccToMacroNum[data1];
+        
+        if(macroNum != undefined)
+        {
+            incrementEncoder(macroNum, data2);
         }
-
-        if (rotaries.isRotary(data1)) {
-            rotaries.onMidi(status, data1, data2);
-        } else if (isUserControlCC(data1)) {
-            var index = ccToUserIndex[data1];
-            userControls.getControl(index).set(data2, 128);
-        }
-    }
-
-    // Pads
-    if (MIDIChannel(status) == 9 && isNoteOn(status) && data2 > 10) {
-        switch (data1) {
-            case PADS.C14:
-                rotaries.toggleMode();
-                break;
-            case PADS.C15:
-                break;
-            case PADS.C16:
-                break;
-            case PADS.C17:
-                break;
-            case PADS.C18:
-                break;
-            case PADS.C19:
-                break;
-            case PADS.C20:
-                break;
-            case PADS.C21:
-                break;
+        else
+        {
+            processTransportButton(data1);
         }
     }
 }
 
+/*
+ * Handles incrementing and decrementing the encoder.
+ * Since we are using the '2’s Compliment from 64 / Relative (Binary Offset)' encoder mode
+ * Anything above 64 is an increment and anything below it is a decrement.
+ */
+function incrementEncoder(macroNum, ccValue)
+{
+    var midpoint = 64;
+    var resolution = 64;
+    var amount = ccValue - midpoint;
+    cursorControlPage.getParameter(macroNum).inc(amount, resolution)
+}
+
+/*
+ * Process the transport buttons
+ */
+function processTransportButton(cc)
+{
+    switch (cc) {
+        case transportCC.LOOP:
+            cursorDevice.isWindowOpen().toggle();
+            break;
+        case transportCC.REW:
+            cursorTrack.selectPrevious();
+            break;
+        case transportCC.FF:
+            cursorTrack.selectNext();
+            break;
+        case transportCC.STOP:
+            transport.stop();
+            break;
+        case transportCC.PLAY:
+            transport.play();
+            break;
+        case transportCC.REC:
+            transport.record();
+            break;
+    }
+}
+
+/*
+ * Called when the script exits
+ */
 function exit() {
 }
